447dfaa254c81469aec69959482365c744195441
cquercia <cquercia@gmail.com>
2023-01-31 21:59:57 -0300

    Se agrega Proyecto

---
+++ Js/java.js
@@ -0,0 +1,35 @@
+document.getElementById('bt-nombre').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="Cristian Leonardo Quercia";
+
+});
+
+document.getElementById('bt-email').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="cquercia@gmail.com";
+
+});
+
+document.getElementById('bt-fecha').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="12/01/1982";
+
+});
+
+document.getElementById('bt-dire').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="Ascasubi 1035";
+
+});
+
+document.getElementById('bt-tel').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="11-62867171";
+
+});
+
+document.getElementById('bt-pass').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="ZeroCool";
+
+});
